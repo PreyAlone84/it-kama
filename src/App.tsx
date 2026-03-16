4b8ecda2767bf8c451034288cf89df3b586903b7
Vladimir Khlystov <vkhlystov@quintegro.com>
2026-03-16 11:00:34 +0500

    change title

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ export function App() {
   return (
     <>
       <div className="flex flex-col gap-8 w-full justify-center p-8">
+        <h1>Music player fun</h1>
         <div className="flex gap-8 justify-center">
           <TrackList trackId={trackId} setTrackId={setTrackId} />
           <TrackDetail trackId={trackId} />
