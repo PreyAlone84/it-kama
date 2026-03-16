3d272488e6aa1e8c15368680209d906ded2efdbe
Vladimir Khlystov <vkhlystov@quintegro.com>
2026-03-16 12:02:19 +0500

    fix App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,14 +7,11 @@ export function App() {
   const { trackId, setTrackId } = useTrackId();
 
   return (
-    <>
-      <div className="flex flex-col gap-8 w-full justify-center p-8">
-        <h1>Music player fun</h1>
-        <div className="flex gap-8 justify-center">
-          <TrackList trackId={trackId} setTrackId={setTrackId} />
-          <TrackDetail trackId={trackId} />
-        </div>
+    <div className="flex flex-col gap-8 w-full justify-center p-8">
+      <div className="flex gap-8 justify-center">
+        <TrackList trackId={trackId} setTrackId={setTrackId} />
+        <TrackDetail trackId={trackId} />
       </div>
-    </>
+    </div>
   );
 }
